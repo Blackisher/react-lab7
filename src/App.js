213acc652c6308cf86c0301090de022dada60ec3
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-11-21 09:33:29 +0100

    LAB7Task1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-      Empty App
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            employee: [],
+            isLoading: false
+        }
+    }
+
+    componentDidMount() {
+      this.load()
+    }
+
+    load() {
+      this.setState({isLoading: true})
+      //alternative to fetch is 'axios' - benefit - add error handling
+      fetch('http://localhost:3004/employees')
+          .then(response => response.json())
+          .then(data => this.setState({ employee: data }))
+          .then(()=>this.setState({isLoading: false}));
+    }
+
+    render() {
+        return (<div>
+            LAB7task1
+            Using mockserver from previous excercise print the list of employees along with their age. Indicate (for
+            example
+            with different colour) those that are not active (parameter ‘active’ is set to false).
+            <hr/>
+            Active are green, not active red <hr/>
+            {this.state.isLoading ? "LOADING" :
+                this.state.employee.map(employee =>
+                  <li className={`active${employee.isActive}`} key={employee._id}>{employee.name}, {employee.age}</li>
+                )}
+        </div>)
+    };
 }
+
 
 export default App;
